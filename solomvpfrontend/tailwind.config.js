3969ce3e752202e677a429961996bc37be66a56c
juliwithoutthee <jnboucree@gmail.com>
2022-05-10 08:38:05 +0900

    Updates tailwind config

--- solomvpfrontend/tailwind.config.js
+++ solomvpfrontend/tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}", './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
   theme: {
     extend: {},
   },
