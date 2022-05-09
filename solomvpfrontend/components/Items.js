61556afd34ea37160bfab62a88e4cb4aaa53ab85
juliwithoutthee <jnboucree@gmail.com>
2022-05-09 15:10:20 +0900

    Updates components and adds Payment component

--- solomvpfrontend/components/Items.js
+++ solomvpfrontend/components/Items.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import axios from 'axios';
 
 export default function Items() {
 
+    const getAllItems = async () => {
+        const res = await axios.get('http://localhost:8080/api/items');
+        const data = res.data;
+        console.log(data)
+    }
+
     return (
         <>
-        
+            <img src="./images/Brownies.jpeg" />
+            <img src="./images/CarrotCake.webp" />
+            <img src="./images/ApplePie.jpeg" />
+            <img src="./images/ChocolateChipCookies.jpeg" />
+            <img src="./images/StrawberryShortCake.jpeg" />
         </>
     )
 }
