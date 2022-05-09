4ec2fd7baa95dbb26aa23cfe1bdb3bb29079df80
juliwithoutthee <jnboucree@gmail.com>
2022-05-09 14:10:24 +0900

    Imports Nav into index.js

--- solomvpfrontend/pages/index.js
+++ solomvpfrontend/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 //import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Nav from "../components/Nav.js"
+import Nav from "../components/Nav"
 export default function Home() {
   return (
     <div className={styles.container}>
