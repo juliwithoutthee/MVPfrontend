fab4866fd5e1400a3a0e2da7419d98ca1c7b151e
juliwithoutthee <jnboucree@gmail.com>
2022-05-10 14:31:05 +0900

    Adds useState and useEffect to use for Items

--- solomvpfrontend/pages/index.js
+++ solomvpfrontend/pages/index.js
@@ -1,11 +1,25 @@
 import Head from 'next/head'
+import { useState, useEffect } from "react";
 //import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Nav from "../components/Nav"
 import Items from '../components/Items'
 import Customer from '../components/Customer'
+
+
+
+
 export default function Home() {
 
+  const [items, setItems] = useState([]);
+
+  const getAllItems = async () => {
+    const res = await axios.get('http://localhost:8080/items').catch(err => console.log(err.res))
+    const data = res.data;
+    console.log(data)
+    setItems(data);
+  }
+  useEffect(() => { getAllItems() }, [])
   return (
     <div className={styles.container}>
       <Head>
@@ -18,11 +32,8 @@ export default function Home() {
         <Nav></Nav>
       </main>
       <div>
-        <Items></Items>
-             
-       
+        <Items items = {items} ></Items>
         <Customer></Customer>
-      
       </div>
     </div>
 
